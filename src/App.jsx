14fcf4997f2d81e196543301fba4fb2f22caa4eb
mustakim6 <mustakimbillahh2001@gmail.com>
2025-12-28 13:38:55 +0600

    create a simple app name my pets:exploring array of objects and map, little bit styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,131 @@
+import React from "react"
+import './App.css'
 
 
 function App() {
 
-  return (
-    <>
-    <h1>Hello world</h1>
-    <p>Add some line after deploy in vercel.</p>
-    </>
-  )
+  // const jsx = <h1>its a plain js function and inside that we have bunch of statements.its can be export from this file so that later can import to another file. </h1>
+  // return jsx;
+
+  
+  //React.createElement('type', 'attr/prpperties', 'children')
+//  const elem = React.createElement('h1', null, 'sdkf')
+
+//   const jsx = <h1>jsx is awesome </h1>
+//   console.log(jsx);
+//   // return jsx
+
+//   console.log(elem);
+
+const topic = "JSX";
+const likes = ['jsx', 'React', 'Redux']
+
+
+const pets = [
+  {
+    name: "Whiskers",
+    type: "Cat",
+    description: "A playful and curious orange tabby who loves chasing laser dots.",
+    skills: ["Jumping", "Playing", "Climbing"],
+    image: ""
+  },
+  {
+    name: "Luna",
+    type: "Cat",
+    description: "A calm and affectionate cat who enjoys quiet naps.",
+    skills: ["Sleeping", "Cuddling", "Purring"],
+    image: ""
+  },
+  {
+    name: "Shadow",
+    type: "Cat",
+    description: "A shy black cat with sharp instincts and silent movements.",
+    skills: ["Hiding", "Night vision", "Hunting toys"],
+    image: ""
+  },
+]
+
+
+return(
+
+<>
+
+<div className="App">
+<h1>My Pets {pets.length}</h1>
+<ul className="container">
+  {
+    pets.map( (pet, index)=>{
+      return <li key={index}>
+        <h2>{pet.name}</h2>
+        <p>{pet.description}</p>
+        <img src={ pet.image } 
+        alt={pet.name} 
+        width='100px'
+        height='100px'       
+        />
+        <ul className="skills">
+          {
+          pet.skills.map((skill)=>{
+            return <li key={skill}>{skill}</li>
+          })
+        }
+        </ul>
+      </li>
+    })
+  }
+</ul>
+</div>
+
+
+
+
+
+
+
+
+
+//about jsx
+<div>
+
+  <h1>{topic} is awesome</h1>
+  <h2>{likes}</h2>
+  {/* to print boolean need casting */}
+  <h2>{String(true)}</h2>
+
+{/* we cant direct use if-else in jsx use ternary instead of if-else*/}
+
+{/* {
+  false?
+    <h3>test</h3>
+  :
+    <h3>testasd</h3>
+
+} */}
+
+{/* loops are not works on jsx , instead of this we can use map method */}
+
+{
+  likes.map((like )=> {
+    let likes = like+"M"
+    for(let i = 0; i<likes.length; i++){
+      let output = likes+2
+      return <h3 key={like}>I like {output}</h3>
+    }
+       
+  })
+}
+
+{/*so, everything in jsx allowed/evaluate as value */}
+
+
+
+
+</div>
+</>
+
+)
+
+  
 }
 
 export default App
